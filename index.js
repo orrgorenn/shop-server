484cc4e176eaa18cff5f60660f31534e37ebe738
Orr Goren <orr.goren@zoominfo.com>
2023-03-11 20:24:41 +0200

    chore: mongoose and server config

--- index.js
+++ index.js
@@ -27,3 +27,12 @@ app.use('/client', clientRoutes);
 app.use('/general', generalRoutes);
 app.use('/management', managementRoutes);
 app.use('/sales', salesRoutes);
+
+/* MONGOOSE & SERVER CONFIG */
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
+}).catch((err) => console.error(err))
